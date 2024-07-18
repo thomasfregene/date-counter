2c3b4c5622586daa96e5323b9ce05f8443d00758
thomasfregene <fregzythomas@gmail.com>
2024-07-18 20:27:37 +0100

    restructuring the input fields

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import { useState } from "react";
 
 export default function App() {
-  return <Counter />;
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
 }
 
 function Counter() {
   const [counter, setCount] = useState(0);
   const [step, setStep] = useState(1);
-
-  function handlePreviousStep() {
-    setStep((s) => s - 1);
-  }
-
-  function handleNextStep() {
-    setStep((s) => s + 1);
-  }
 
   const date = new Date();
   date.setDate(date.getDate() + counter);
@@ -22,16 +18,24 @@ function Counter() {
   return (
     <>
       <div>
-        <button onClick={handlePreviousStep}>-</button>
+        <input
+          type="range"
+          min={0}
+          max={10}
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
         <span>Step: {step}</span>
-        <button onClick={handleNextStep}>+</button>
       </div>
       <div>
         <button onClick={() => setCount((c) => c - step)}>-</button>
-        <span>Counter: {counter}</span>
+        <input
+          type="text"
+          value={counter}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
         <button onClick={() => setCount((c) => c + step)}>+</button>
       </div>
-
       <p>
         <span>
           {counter === 0
